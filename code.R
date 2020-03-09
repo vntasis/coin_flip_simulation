454f8e4f947636b453ae8650c04b36a96ef419db
vntasis <vasilisntasis94@gmail.com>
2020-03-09 10:23:38 +0100

    Update the code for the coin flip simulation animation.

--- code.R
+++ code.R
@@ -10,10 +10,10 @@ library(tibble)
 library(dplyr)
 
 
-n <- 40
-n_trials <- 100000
+n <- 20
+n_trials <- 1000
 flips_freq <- 
-  tibble(Number_of_heads=as.factor(1:n), frequency=rep(0,n), trials=as.character(rep(0,n)))
+  tibble(Number_of_heads=as.factor(1:n), frequency=rep(0,n), trials=as.integer(rep(0,n)))
 
 flips <- rbinom(n_trials, n, 0.5)
 
@@ -21,10 +21,10 @@ for (i in 1:n_trials){
   if (i%%1000 == 0) print(i)
 
   flps <-
-    tibble(Number_of_heads = factor(flips[1:i], levels=as.character(1:n))) %>% 
+    tibble(Number_of_heads = factor(as.character(flips[1:i]), levels=as.character(1:n))) %>% 
     group_by(Number_of_heads, .drop = FALSE) %>% 
     summarise(frequency= n()) %>%
-    mutate(trials=as.character(rep(i,n)))
+    mutate(trials=as.integer(rep(i,n)))
 
   flips_freq <-
     rbind(flips_freq, flps)
@@ -32,15 +32,10 @@ for (i in 1:n_trials){
 }
 
 
-flips_freq %>%
+g <- flips_freq %>%
   ggplot(aes(Number_of_heads, frequency)) +
-  geom_segment( aes(x=Number_of_heads , xend=Number_of_heads, y=0, yend=frequency), color="grey") +
-  geom_point( color="orange", size=4) +
-  transition_states(
-    trials,
-    transition_length = 2,
-    state_length = 1
-  ) +
-  enter_fade() +
-  exit_shrink() +
-  ease_aes('sine-in-out')
+  geom_bar(stat = 'identity') + 
+  transition_states(trials) +
+  labs(title = "Trials: {closest_state}")
+
+#animate(g + enter_fade() + exit_shrink(), fps = 50, nframes = 2000)
